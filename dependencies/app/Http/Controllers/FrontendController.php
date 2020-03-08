7d639c6e0a111440edb250ecf297813f45a8e0e3
thailanddegito <46433919+thailanddegito@users.noreply.github.com>
2020-03-09 03:22:43 +0700

    update

--- dependencies/app/Http/Controllers/FrontendController.php
+++ dependencies/app/Http/Controllers/FrontendController.php
@@ -26,12 +26,20 @@ class FrontendController extends Controller
   {
     $pages = DB::table('pages')->where('slug', $slug)->first();
     $page_id = $pages->page_id;
-    $widgets = DB::table('widget')
-        ->join('widget_content as wm', 'widget.widget_id', '=', 'wm.widget_fk_Id')
-        ->where('widget.page_fk_id', $page_id)
-        ->select('widget.*', 'wm.title', 'wm.content','wm.image', 'wm.link', 'wm.img_alt', 'wm.color_title', 'wm.youtube_link')
-        ->orderBy('order_widget', 'asc')
-        ->get();
+    // $widgets = DB::table('widget')
+    //     ->join('widget_content as wm', 'widget.widget_id', '=', 'wm.widget_fk_Id')
+    //     ->where('widget.page_fk_id', $page_id)
+    //     ->select('widget.*', 'wm.title', 'wm.content','wm.image', 'wm.link', 'wm.img_alt', 'wm.color_title', 'wm.youtube_link')
+    //     ->orderBy('order_widget', 'asc')
+    //     ->get();
+    $widgets = DB::table('widget')->where('widget.page_fk_id', $page_id)->orderBy('order_widget', 'asc')->get();
+    for($i = 0 ; $i < count($widgets) ; $i++) {
+      $widget_contents = DB::table('widget_content')
+      ->where('widget_content.widget_fk_Id', $widgets[$i]->widget_id)
+      ->get();
+      
+      $widgets[$i]->items = $widget_contents;
+    }
       return response()->json([
           'widgets' => $widgets,
       ], 200);
